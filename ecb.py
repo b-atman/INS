4595f534830b2c2eb78e958e4d529bc684f32458
Atman Italiya <80062496+b-atman@users.noreply.github.com>
2023-05-17 08:14:44 +0530

    Update ecb.py

--- ecb.py
+++ ecb.py
@@ -1,58 +1,77 @@
-import multiprocessing
-import time
+def text_to_binary(text):
+    # Convert plaintext to binary string
+    return ''.join(format(ord(c), '08b') for c in text)
 
-para = "In ECB mode, the plaintext is divided into blocks of fixed length, typically 64 or 128 bits, and each block is encrypted independently using the same key. The encryption of each block is performed in isolation, so the encryption of one block does not affect the encryption of the next block. The resulting ciphertext blocks are then concatenated to produce the encrypted message."
+def binary_to_text(binary):
+    # Convert binary string to plaintext
+    return ''.join(chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8))
 
+def ecb_encrypt(plaintext, key, t, mode):
+    # Convert plaintext to binary
+    binary_plaintext = text_to_binary(plaintext)
 
-def decToBin(dec):
-    return bin(dec).replace("0b", "")
+    # Pad the binary plaintext to a multiple of 128 bits
+    padding_length = 128 - len(binary_plaintext) % 128
+    binary_plaintext += '0' * padding_length
 
+    # Split binary plaintext into 128-bit blocks
+    blocks = [binary_plaintext[i:i+128] for i in range(0, len(binary_plaintext), 128)]
 
-ascii_vals = [ord(char) for char in para]
-binary_vals = [decToBin(num) for num in ascii_vals]
-combined = ""
-for el in binary_vals:
-    combined += el
+    # Encrypt each block using ECB mode and key
+    ciphertext_blocks = []
+    for block in blocks:
+        if mode == 'left':
+            block = block[t:] + block[:t]  # Shift t bits to the left
+        elif mode == 'right':
+            block = block[-t:] + block[:-t]  # Shift t bits to the right
 
-blocks = []
-start = 0
-end = len(combined)
-for i in range(start, end, 128):
-    x = i
-    blocks.append(combined[x:x + 128])
-    if len(blocks[len(blocks) - 1]) < 128:
-        shortBlock = blocks[len(blocks) - 1]
-        while len(shortBlock) != 128:
-            shortBlock += '0'
-        blocks[len(blocks) - 1] = shortBlock
+        
 
+        ciphertext_blocks.append(block)
 
-def shiftLeft(arr, t):
-    lFirst = arr[0:t]
-    lSec = arr[t:]
-    return lSec + lFirst
+    # Concatenate the ciphertext blocks and convert to plaintext
+    ciphertext_binary = ''.join(ciphertext_blocks)
+    ciphertext = binary_to_text(ciphertext_binary)
 
+    return ciphertext
 
-class Process(multiprocessing.Process):
+# Example usage
+plaintext = "This is a sample plaintext to be encrypted using ECB mode."
+key = "secretkey"
+t = 3
+mode = 'left'
 
-    def __init__(self, arr, t):
-        super(Process, self).__init__()
-        self.arr = arr
-        self.t = t
+ciphertext = ecb_encrypt(plaintext, key, t, mode)
 
-    def run(self):
-        lFirst = self.arr[0:self.t]
-        lSec = self.arr[self.t:]
-        binString = (lSec + lFirst)
-        binString += '00000'
-        n = 7
-        chunks = [binString[i:i + n] for i in range(0, len(binString), n)]
-        for chunk in chunks:
-            ascii_int = int(chunk, 2)
-            print(chr(ascii_int), end="")
+print("Plaintext: ", plaintext)
+print("Ciphertext:", ciphertext)
 
+def ecb_decrypt(ciphertext, key, t, mode):
+    # Convert ciphertext to binary
+    binary_ciphertext = text_to_binary(ciphertext)
 
-for elem in blocks:
-    p = Process(elem, 3)
-    p.start()
-    p.join()
+    # Split binary ciphertext into 128-bit blocks
+    blocks = [binary_ciphertext[i:i+128] for i in range(0, len(binary_ciphertext), 128)]
+
+    # Decrypt each block using ECB mode and key
+    plaintext_blocks = []
+    for block in blocks:
+        decrypted_block = block
+        if mode == 'left':
+            decrypted_block = decrypted_block[-t:] + decrypted_block[:-t]  # Shift t bits to the right
+        elif mode == 'right':
+            decrypted_block = decrypted_block[t:] + decrypted_block[:t]  # Shift t bits to the left
+
+        plaintext_blocks.append(decrypted_block)
+
+    # Concatenate the decrypted blocks and convert to plaintext
+    plaintext_binary = ''.join(plaintext_blocks)
+    plaintext = binary_to_text(plaintext_binary)
+
+    return plaintext
+
+# Example usage
+decrypted_text = ecb_decrypt(ciphertext, key, t, mode)
+
+print("Ciphertext:", ciphertext)
+print("Decrypted text:", decrypted_text)
